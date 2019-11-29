e8d519645ef7dbfba9a2bffbdce3c34032f6af27
Gad Ishimwe <coolshigad@gmail.com>
2019-11-29 15:15:55 +0200

    fixing staring the app after db connection

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
 db.authenticate()
   .then(() => {
     console.log('Connection has been established successfully.....');
-    app.listen(PORT, console.log(`Listening on port ${PORT}.......`));
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}.......`));
   })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+  .catch(err => console.error('Unable to connect to the database:', err));
