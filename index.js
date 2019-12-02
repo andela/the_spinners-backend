81e8ae53b2192a909492725eeb05771b057abf51
ABIZEYIMANA Victor <svicky.shema@gmail.com>
2019-12-02 09:43:10 +0200

    chore(swagger): Setting up swagger
    Setup swagger with JSDoc
    
    [start #169817535]

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import { serve, setup } from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerDefinition from './src/swagger';
 import db from './src/config/dbConfig';
 
 const app = express();
+
+const swaggerOptions = {
+  customSiteTitle: 'The Spinners',
+  explorer: true,
+};
+
+const swaggerSpecs = swaggerJsdoc(swaggerDefinition);
+app.use('/docs', serve, setup(swaggerSpecs, swaggerOptions));
 
 const PORT = process.env.PORT || 3000;
 
---
+++ src/swagger/index.js
@@ -0,0 +1,18 @@
+// Swagger set up
+const swaggerDefinition = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Barefoot Nomad(The Spiners)',
+      version: '1.0.0',
+      description:
+        'A platform to make company global travel and accommodation easy and convenient for the strong workforce of savvy members of staff, by leveraging the modern web.'
+    },
+    host: 'localhost:1234', // the host or url of the app
+    basePath: '/api/v1', // the basepath of your endpoint
+  },
+  // List of apis
+  apis: ['./routes/*.js']
+};
+
+export default swaggerDefinition;
