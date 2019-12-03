209680cc41de770bb4d58b1c040ceecc9390250d
GUSTAVE <58309608+higustave-ops@users.noreply.github.com>
2019-12-03 12:00:35 +0200

    #169817532 Setup unit test

---
+++ src/tests/index.js
@@ -0,0 +1,20 @@
+import chai from 'chai';
+
+import chaiHttp from 'chai-http';
+
+chai.should();
+chai.use(chaiHttp);
+
+const dummyData = [{ Name: 'Gustave', Age: 26 }, { Name: 'Peter', Age: 23 }, { Name: 'Anne', Age: 20 }];
+
+describe('Test on the array:', () => {
+  it('Should return array type', (done) => {
+    dummyData.should.be.an('array');
+    done();
+  });
+
+  it('Should return length of array equal to 3', (done) => {
+    dummyData.should.have.lengthOf(3);
+    done();
+  });
+});
