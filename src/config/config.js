9c0147ec43fe27aa4580e057f2bdc81d68cd6cff
Gad Ishimwe <coolshigad@gmail.com>
2019-12-05 11:26:34 +0200

    using URL to migrate and removing unecessary changes from .eslintrc

--- src/config/config.js
+++ src/config/config.js
@@ -3,23 +3,14 @@ import { config } from 'dotenv';
 config();
 
 export const development = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOSTNAME,
+  url: process.env.DB_URL,
   dialect: 'postgres',
 };
 export const test = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOSTNAME,
+  url: process.env.DB_URL,
   dialect: 'postgres',
 };
 export const production = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOSTNAME,
+  url: process.env.DB_URL,
   dialect: 'postgres',
 };
