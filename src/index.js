6db18d668f880e8b40e543ecb80d2bb967bb60e7
Gad Ishimwe <coolshigad@gmail.com>
2019-12-05 10:16:06 +0200

    removed db authentication from index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { serve, setup } from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerDefinition from './swagger';
-import db from './config/dbConfig';
 
 const app = express();
 
@@ -16,8 +15,5 @@ app.use('/docs', serve, setup(swaggerSpecs, swaggerOptions));
 
 const PORT = process.env.PORT || 3000;
 
-db.authenticate()
-  .then(() => {
-    // eslint-disable-next-line no-console
-    app.listen(PORT, () => console.log(`Listening on port ${PORT}.......`));
-  });
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.log(`Listening on port ${PORT}.......`));
