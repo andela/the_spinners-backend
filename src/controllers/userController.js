c80a47d924bbdfa3b0660b845d9e8daf74a89c92
Gustave Harintwari <higustave123@gmail.com>
2019-12-06 17:12:27 +0200

    Change to async await

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,24 +2,30 @@ import model from '../models';
 
 const { Users } = model;
 
+/**
+ * Adds two numbers together.
+ * @param {int} num1 The first number.
+ * @param {int} num2 The second number.
+ * @returns {int} The sum of the two numbers.
+ */
+
 class UsersController {
-  static signUp(req, res) {
-    if (!req.body.firstName || !req.body.lastName || !req.body.username || !req.body.email || !req.body.password) {
-      return res.status(400).send({
-        status: 400,
-        error: 'All fields are required'
-      });
-    } 
-      const { firstName, lastName, username, email, password } = req.body;
-      return Users
-        .create({ firstName, lastName, username, email, password 
-})
-        .then(userData => res.status(201).send({
-          status: 201,
-          message: 'User successfully created',
-          userData
-        }));
-    
+  static async signUp(req, res) {
+    if (!req.body.firstName || !req.body.lastName || !req.body.username
+      || !req.body.email || !req.body.password) {
+        return res.status(400).send({
+          status: 400,
+          error: 'Operation not performed. All fields are required'
+        })
+    }else{
+      const newUser = req.body;
+      const createdUser = await Users.create(newUser);
+      return res.status(200).send({
+        status: 200,
+        message: 'User created successfully',
+        data: createdUser
+      })
+    }
   }
 }
 
