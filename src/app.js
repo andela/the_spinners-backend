22523494462d99d7be36fcaff4a608d0d8e14724
Gad Ishimwe <coolshigad@gmail.com>
2020-02-04 10:21:49 +0200

    chore(Cors): Add cors [starts #171074205]

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@
 import express from 'express';
 import morgan from 'morgan';
 import passport from 'passport';
+import cors from 'cors';
 import routes from './routes/index';
 import NotificationListeners from './helpers/notifications/index';
 
 const app = express();
 app.use(morgan('dev'));
-
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(passport.initialize());
