b1a20bbdcbc80dd871adbfa0932cf181b139e434
Iragena Eric <erickyvand@gmail.com>
2020-02-20 17:43:07 +0200

    feature(get users): add users
    - get all users
    
    [Starts #171380499]

--- src/routes/profile-settings.route.js
+++ src/routes/profile-settings.route.js
@@ -11,6 +11,7 @@ import assignManagerValidation from '../validations/assign-manager.validation';
 const router = express.Router();
 
 router.get('/view-profile', RouteAccessMiddleware.checkRouteAccess, validateToken, ProfileSettingsController.viewProfile);
+router.get('/settings/view-users-roles', authMiddleware.checkUserLoggedIn, authMiddleware.verifyIfUserIsAdminById, SettingsController.ListUsersRoles);
 router.patch('/edit-profile', RouteAccessMiddleware.checkRouteAccess, validateToken, validateAccountProfile, ProfileSettingsController.editProfile);
 router.patch('/settings/roles', authMiddleware.checkUserLoggedIn, validateUserRole, authMiddleware.verifyIfUserIsAdmin, SettingsController.changeUserRole);
 router.patch('/:userId', authMiddleware.checkUserLoggedIn, authMiddleware.verifyIfUserIsAdmin, assignManagerValidation, SettingsController.assignRequesterToManager);
---
+++ src/tests/settings/users.test.js
@@ -0,0 +1,59 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app';
+import { superAdminToken1, nonManagerToken2 } from '../fixtures/managers.fixture';
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('/Get View Users', () => {
+  it('Should get all users and their respective roles', (done) => {
+    chai.request(app)
+      .get('/api/users/settings/view-users-roles?page=1&limit=1')
+      .set('Authorization', superAdminToken1)
+      .end((err, res) => {
+        res.body.should.be.an('object');
+        res.status.should.be.equal(200);
+        res.body.data.should.have.property('pageMeta');
+        res.body.data.should.have.property('rows');
+        res.body.data.rows.should.be.an('array');
+        res.body.data.rows[0].should.have.property('firstName');
+        res.body.data.rows[0].should.have.property('lastName');
+        res.body.data.rows[0].should.have.property('email');
+        res.body.data.rows[0].should.have.property('role');
+        res.body.should.have.property('message').equal('List of users and their respective roles');
+        done();
+      });
+  });
+
+  it('Should get all users and their respective roles', (done) => {
+    chai.request(app)
+      .get('/api/users/settings/view-users-roles')
+      .set('Authorization', superAdminToken1)
+      .end((err, res) => {
+        res.body.should.be.an('object');
+        res.status.should.be.equal(200);
+        res.body.data.should.have.property('pageMeta');
+        res.body.data.should.have.property('rows');
+        res.body.data.rows.should.be.an('array');
+        res.body.data.rows[0].should.have.property('firstName');
+        res.body.data.rows[0].should.have.property('lastName');
+        res.body.data.rows[0].should.have.property('email');
+        res.body.data.rows[0].should.have.property('role');
+        res.body.should.have.property('message').equal('List of users and their respective roles');
+        done();
+      });
+  });
+
+  it('Should get all users and their respective roles', (done) => {
+    chai.request(app)
+      .get('/api/users/settings/view-users-roles?page=1&limit=1')
+      .set('Authorization', nonManagerToken2)
+      .end((err, res) => {
+        res.body.should.be.an('object');
+        res.status.should.be.equal(403);
+        res.body.should.have.property('message').equal('You are not a super admin.Action not performed');
+        done();
+      });
+  });
+});
