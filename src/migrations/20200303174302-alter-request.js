17dec0536d481aa5028df1402d805d784e528026
Gustave Harintwari <higustave123@gmail.com>
2020-03-03 16:49:09 +0200

    add fields to requests table Fix hound errors

---
+++ src/migrations/20200303174302-alter-request.js
@@ -0,0 +1,36 @@
+/**
+ * @exports
+ * @class
+ * @param {object} queryInterface
+ * @param {object} Sequelize
+ */
+export function up(queryInterface, Sequelize) {
+  return queryInterface.sequelize.transaction(t => Promise.all([
+    queryInterface.addColumn('Requests', 'requesterFname', {
+      type: Sequelize.DataTypes.STRING
+    }, { transaction: t }),
+    queryInterface.addColumn('Requests', 'requesterLname', {
+      type: Sequelize.DataTypes.STRING,
+    }, { transaction: t }),
+    queryInterface.addColumn('Requests', 'requesterPicture', {
+      type: Sequelize.DataTypes.STRING
+    }, { transaction: t }),
+    queryInterface.addColumn('Requests', 'tripType', {
+      type: Sequelize.DataTypes.STRING
+    }, { transaction: t })
+  ]));
+}
+/**
+ * @exports
+ * @class
+ * @param {object} queryInterface
+ * @param {object} Sequelize
+ */
+export function down(queryInterface) {
+  return queryInterface.sequelize.transaction(t => Promise.all([
+    queryInterface.removeColumn('Requests', 'requesterFname', { transaction: t }),
+    queryInterface.removeColumn('Requests', 'frequesterLname', { transaction: t }),
+    queryInterface.removeColumn('Requests', 'requesterPicture', { transaction: t }),
+    queryInterface.removeColumn('Requests', 'tripType', { transaction: t })
+  ]));
+}
