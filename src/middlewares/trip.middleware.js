50b9115e34222e0f5013ad81b955db1819f4109f
Iragena Eric <erickyvand@gmail.com>
2020-03-12 19:02:39 +0200

    chore(fix view comments): add view comments
    - add view comments
    
    [Starts #171746475]

--- src/middlewares/trip.middleware.js
+++ src/middlewares/trip.middleware.js
@@ -8,30 +8,6 @@ import TripService from '../services/trip.service';
  */
 class TripMiddleware {
   /**
-   *
-   *
-   * @static
-   * @param {req} req
-   * @param {res} res
-   * @param {next} next
-   * @returns {object} This function check if trip ID exists
-   * @memberof TripMiddleware
-   */
-  static async checkTripExist(req, res, next) {
-    const trip = await TripService
-      .findTripByProperty({
-        id: req.params.tripId,
-        userId: req.userData.id
-      });
-
-    if (!trip) {
-      ResponseService.setError(404, 'Trip ID does not exists or you does not belong to the trip ID');
-      return ResponseService.send(res);
-    }
-    next();
-  }
-
-  /**
  *
  *
  * @static
