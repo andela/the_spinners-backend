f13568e2e58380c31a20ec15185b7bbce54d631c
victor-abz <52409462+victor-abz@users.noreply.github.com>
2020-03-26 15:00:40 +0200

    cg(socket): Fix socket early disconnect (#88)

--- src/models/trip.js
+++ src/models/trip.js
@@ -21,8 +21,5 @@ export default (sequelize, DataTypes) => {
   Trip.afterCreate(({ dataValues }) => {
     emitter.emit('request-created', dataValues);
   });
-  Trip.afterUpdate((data) => {
-    emitter.emit('request-updated', data);
-  });
   return Trip;
 };
